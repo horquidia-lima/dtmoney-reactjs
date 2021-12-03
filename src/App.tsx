5b4d049dc3cd52496c636436e15c46966a475b5f
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-02 22:07:40 -0300

    component summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
 import {GlobalStyle} from "./styles/global";
+import {Deshboard} from "./components/Dashboard"
 
 export function App() {
   return (
     <>
       <Header/>
+      <Deshboard/>
       <GlobalStyle/>
     </>
   );
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Summary } from "../Summary";
+
+export function Deshboard(){
+    return(
+        <Container>
+            <Summary/>
+        </Container>
+    )
+}
