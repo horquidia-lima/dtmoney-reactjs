39591c7cbb3bf786c7920ba62f9dd9c4e5369501
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-02 22:49:56 -0300

    component transactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Deshboard(){
     return(
         <Container>
             <Summary/>
+            <TransactionsTable/>
         </Container>
     )
 }
