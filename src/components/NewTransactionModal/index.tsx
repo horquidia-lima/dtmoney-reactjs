1d9a4660a2ab017d4eae7aef0638d3930dbbe0e1
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-06 11:09:06 -0300

    criando botoes de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal"
-import { Container } from "./styles"
+import { Container, TransactionTypeContainer } from "./styles"
 import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
 
 interface NewTransactionModalProps {
     isOpen: boolean
@@ -32,6 +34,23 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 placeholder="Valor"
                 type="number"
             />
+
+            <TransactionTypeContainer>
+                <button
+                    type="button"
+                >
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+                </button>
+
+                <button
+                    type="button"
+                >
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Saída</span>
+                </button>
+            </TransactionTypeContainer>
+
             <input 
                 placeholder="Categoria"
             />
