2673c61e5f391bbefb5b67705cde12e540099481
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-06 22:22:18 -0300

    listando transacoes e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Frelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          creatAt: new Date("2021-02-12 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          creatAt: new Date("2021-02-14 11:00:00")
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = "api"
 
