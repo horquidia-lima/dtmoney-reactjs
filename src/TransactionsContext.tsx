8c7254464ddd64d47debe498e346bba7eb0a53ef
Horquidia Lima <horquidia.lima@inworx.com>
2021-12-08 22:49:45 -0300

    finalizando insercao

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -17,7 +17,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData{
     transactions: Transaction[]
-    createTransaction: (transaction: TransactionInput) => void
+    createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -32,9 +32,18 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
-    function createTransaction(transaction: TransactionInput){
+    async function createTransaction(transactionInput: TransactionInput){
        
-          api.post("/transactions", transaction)
+        const response = await  api.post("/transactions", {
+            ...transactionInput,
+            createdAt: new Date()
+        })
+        const {transaction} = response.data
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
     }
 
     return(
